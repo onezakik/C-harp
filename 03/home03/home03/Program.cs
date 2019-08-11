240e3a981125c10f4e1fe4c928383d5c244dde96
den sushientsiev <dsushientsiev@mail.ru>
2019-08-11 14:43:46 +0300

    ()

---
+++ 03/home03/home03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace home03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 11;
+            for (int i = 1; i < num; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j < num; j++)
+                {
+                    Console.Write("{0} ", i * j);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
