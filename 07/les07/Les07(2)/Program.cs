3c1389c2bc2e8f0b6b0fc0443265b03157ed3e67
Your Name <kmpochinskaya@gmail.com>
2019-08-19 21:59:58 +0300

    ()

--- 07/les07/Les07(2)/Program.cs
+++ 07/les07/Les07(2)/Program.cs
@@ -6,7 +6,43 @@ namespace Les07_2_
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+			//var test = "test string";
+			//Console.WriteLine(test.Contains(" "));
+			//Console.WriteLine(test.StartsWith("te"));
+			//Console.WriteLine(test.EndsWith("z"));
+
+			//Console.WriteLine(test.IndexOf("s", 3));
+			//Console.WriteLine(test.LastIndexOf("s"));
+
+			//Console.WriteLine("Введите  Слово");
+			//var inputworld = Console.ReadLine();
+			//Console.WriteLine("Введите подстроку");
+			//var subString = Console.ReadLine();
+			//int foundPosition = -1;
+			//string answer = string.Empty;
+			//do
+			//{
+			//	foundPosition = inputworld.IndexOf(inputworld, foundPosition + 1);	
+			//	if (foundPosition > 0)
+			//	{
+			//		answer += foundPosition + " ";
+			//	}
+			//}
+			//while (foundPosition >= 0);
+			//if (answer == string.Empty) 
+			//{
+			//	answer = "-";
+			//}
+			//Console.WriteLine(answer == string.Empty
+			//	? "-"
+			//	: answer);
+
+
+
+			Console.ReadKey();
+
+
 		}
 	}
 }
