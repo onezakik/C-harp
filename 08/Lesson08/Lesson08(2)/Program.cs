16d73136f9ac6ac4685447d9cca03a49152a0174
Your Name <kmpochinskaya@gmail.com>
2019-08-21 20:50:10 +0300

    ()

---
+++ 08/Lesson08/Lesson08(2)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson08_2_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//List<int> intList = new List<int>
+			//{
+			//	10,
+			//	20,
+			//	30,
+			//	40
+			//};
+
+			//Console.WriteLine(string.Join(", ", intList));
+			List<double> values = new List<double>();
+
+			Console.WriteLine("Напишите Stop");
+			const string stop = "stop";
+			string input=string.Empty;
+			while(true)
+			{
+				input = (Console.ReadLine());
+				if (input == stop)
+				{
+					break;
+				}
+
+				double value = double.Parse(input);
+				values.Add(value);
+			};
+
+			double sOfValues = 0;
+			foreach(double value in values)
+			{
+				sOfValues += value;
+			}
+			Console.WriteLine(sOfValues);
+			Console.WriteLine(sOfValues / values.Count);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 08/Lesson08/Lesson08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lesson08
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string fileName = @"test.txt";
+			const string testMassage = "Hello world!";
+
+			byte[] massageAnsiBytes = Encoding.ASCII.GetBytes(testMassage);
+
+			FileStream testFileSteam = File.Open(
+				fileName,
+				FileMode.OpenOrCreate,
+				//FileMode.Create,
+				FileAccess.Write, 
+				FileShare.Read);
+
+			testFileSteam.Seek(0, SeekOrigin.End);
+			testFileSteam.Write(massageAnsiBytes);
+			testFileSteam.Flush();
+			testFileSteam.Close();
+			 
+
+			//Console.ReadKey();
+		}
+	}
+}
