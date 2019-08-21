6ac30e567510cf8e43b46489523078e40b7832cf
Your Name <kmpochinskaya@gmail.com>
2019-08-21 22:14:33 +0300

    ()

---
+++ 08/Lesson08/Lesson08(3)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson08_3_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Dictionary<string, string> countries = new Dictionary<string, string>(5);
+			//countries.Add("1", "Russia");
+			//countries.Add(3.ToString(), "Great Britain");
+			//countries.Add("2", "USA");
+			//countries.Add("4", "France");
+			//countries.Add("5", "China");
+
+			//foreach (KeyValuePair<string, string> keyValue in countries)
+			//	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+
+			//string country = countries["4"];
+			//countries["4"] = "Spain";
+			//countries.Remove("2");
+
+			//Console.WriteLine("the most Country in the world is {0}.aff {0} ", countries["5"]);
+
+			Console.WriteLine("Введите команду! wash , dry, exit");
+			string input = Console.ReadLine();
+			Stack<string> stack = new Stack<string>();
+
+			stack.Push("wash");
+			//stack.Push("dry");
+			//stack.Push("exit");
+			while (stack.Count > 0)
+			{
+				if (input == "wash")
+				stack.Push("wash");
+				if (input == "dry")
+				stack.Pop();
+				if (input == "exit") 
+				stack.
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
