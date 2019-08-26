cc3b6132236959e5ca21cbbbfebe52fe9fc962ab
Your Name <kmpochinskaya@gmail.com>
2019-08-26 21:50:32 +0300

    123

--- 09/LEson09/09/09/Program.cs
+++ 09/LEson09/09/09/Program.cs
@@ -12,14 +12,29 @@ namespace _09
 			//int x = 0;
 			//x = x + 1;
 
+			int a = 10;
+			UpgrateValue(ref a);
+			Console.WriteLine(a);
+
 			const int length = 5;
 			const int maxValue = int.MaxValue;
 
 			int[] arr = GetTestArray(length, maxValue);
+			{
+				OutputArray(arr, " исход данные");
 
+				var sortedarr = BubbleSort(arr);
+				OutputArray(sortedarr, "sorted array(bubble):");
 
-			
+				OutputArray(arr, "исход данные");
 
+				OutputArray(arr, "отсортировка");
+
+				var sortedArr2 = Systemsort(arr);
+				OutputArray(sortedArr2, " сортировка (система):");
+
+			}
+			Console.ReadKey();
 
 			for (int i = 0; i < arr.Length; i++)
 			{
@@ -39,9 +54,21 @@ namespace _09
 					}
 				}
 			}
+
 			Console.WriteLine(string.Join(", ", arr));
-			Console.ReadKey();
+
 		}
+
+		private static void OutputArray(object sortedArr2, string v)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static object Systemsort(int[] arr)
+		{
+			throw new NotImplementedException();
+		}
+
 		public static int[] GetTestArray(int arrayLenght, int maxElemenntValue)
 		{
 			var arr = new int[arrayLenght];
@@ -52,6 +79,39 @@ namespace _09
 				arr[i] = rnd.Next(maxElemenntValue);
 			}
 			return arr;
+		}
+		public static void OutputArray(int[] arr, string message)
+		{
+			Console.WriteLine(message);
+			Console.WriteLine(string.Join(", ", arr) + "\n");
+		}
+		public static int[] BubbleSort(int[] inputArr)
+		{
+			int[] arr = (int[])inputArr.Clone();
+			for (int j = 0; j < arr.Length - 1; j++)
+			{
+				for (int i = 0; i < arr.Length - 1 - j; i++)
+				{
+					if (arr[i] > arr[i + 1])
+					{
+						int temp = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = temp;
+					}
+				}
+			}
+			return arr;
+		}
+		public static void UpgrateValue(ref int a)
+		{
+			a++;
+			Console.WriteLine(a);
+		}
+		private static[] SystemSort(int[] arr)
+		{
+			int[] a=int[] arr.Clone());
+			Array.Sort(a);
+			return a;
 		}
 	}
 }
